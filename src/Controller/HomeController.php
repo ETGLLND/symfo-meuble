93b08284eec1aa5f58095811c0470af22a35d15b
Pierre-J Backes <pj.backes@gmail.com>
2021-06-22 12:01:26 +0200

    add folder templates/home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\FurnitureRepository;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {

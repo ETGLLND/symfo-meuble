d77a7757ac953c6e8ab5e487fc52ecbee9ecfc5d
Pierre-J Backes <pj.backes@gmail.com>
2021-06-22 16:57:23 +0200

    creation of CategoryController

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category/{id}", name="show_category")
+     */
+    public function show(CategoryRepository $categoryRepository, int $id)
+    {
+        $category = $categoryRepository->find($id);
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category
+        ]);
+    }
+}

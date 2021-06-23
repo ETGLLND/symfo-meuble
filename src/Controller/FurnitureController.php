972d50989d54c1704e89c7874362512f25ca318c
Etienne Galland <etienne.galland@mataf.net>
2021-06-23 10:25:45 +0200

    Create form to add a new piece of furniture

--- src/Controller/FurnitureController.php
+++ src/Controller/FurnitureController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Furniture;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Material;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FurnitureController extends AbstractController
 {
@@ -18,6 +23,37 @@ class FurnitureController extends AbstractController
         $furniture = $repository->find($id);
         return $this->render("furniture/index.html.twig", [
             "furniture" => $furniture
+        ]);
+    }
+
+    /**
+     * @Route("/furniture_add", name="furniture_add")
+     */
+    public function add()
+    {
+        $form = $this->createFormBuilder(null, ['data_class' => Furniture::class])
+            ->add('name', TextType::class, [
+                'label' => 'Nom du meuble : '
+            ])
+            ->add('craft_number', IntegerType::class, [
+                'label' => 'Nombre de fois fabriqué : '
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie du meuble : ',
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+            ->add('materials', EntityType::class, [
+                'label' => 'Matériau du meuble : ',
+                'class' => Material::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->getForm();
+        
+        return $this->render("furniture/add.html.twig", [
+            "form" => $form->createView()
         ]);
     }
 }

d63f7f65ce22508cc51b6c42d154358420836b8d
Pierre-J Backes <pj.backes@gmail.com>
2021-06-23 14:55:07 +0200

    Creation of category form

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Nom de la catégorie'
+                ]
+            ]);
+    }
+}
